2098a2aa1d1344f43573f7afda34c9cdf39e5df7
Temurmalik <temurmalikabbosov@yandex.ru>
2022-04-10 05:57:53 +0500

    change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Wooden Spoon");
         System.out.println("Java is fun");
+        System.out.println("Cydeo");
     }
 }
