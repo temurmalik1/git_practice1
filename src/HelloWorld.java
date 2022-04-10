dc3fed87d7c96c1b725b83ea92b74c102c031fa5
Temurmalik <temurmalikabbosov@yandex.ru>
2022-04-10 05:54:46 +0500

    added more work

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Wooden Spoon");
+        System.out.println("Java is fun");
     }
 }
